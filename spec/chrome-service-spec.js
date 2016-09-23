72cc066ef13cac71739a31bc3f8b94d281877c00
Rafael Camargo <contato@rafaelcamargo.com>
2016-09-23 00:26:41 -0300

    Covered first half of chrome service module with unit tests

---
+++ spec/chrome-service-spec.js
@@ -0,0 +1,96 @@
+describe('Chrome Service', function(){
+
+  var SUCCESS_GET_ALARM_RESPONSE = {
+    id: 36,
+    value: '63'
+  };
+
+  beforeEach(function(){
+    spyOn(chrome.alarms, 'get');
+    spyOn(chrome.alarms, 'clear');
+  });
+
+  it('should provide an interface to alarms api', function(){
+    expect(chromeService.alarms).toBeDefined();
+  });
+
+  it('should provide an interface to storage api', function(){
+    expect(chromeService.storage).toBeDefined();
+  });
+
+  it('should create an alarm', function(){
+    spyOn(chrome.alarms, 'create');
+
+    chromeService.alarms.create(36, {
+      value: '63'
+    });
+    expect(chrome.alarms.create).toHaveBeenCalledWith(36, {
+      value: '63'
+    });
+  });
+
+  it('should add an alarm listener', function(){
+    spyOn(chrome.alarms.onAlarm, 'addListener');
+
+    var callback = function(){};
+    chromeService.alarms.addListener(callback);
+    expect(chrome.alarms.onAlarm.addListener).toHaveBeenCalledWith(callback);
+  });
+
+  it('should get an alarm with a given callback', function(){
+    var callback = function(){};
+    chromeService.alarms.get(36, callback);
+    expect(chrome.alarms.get).toHaveBeenCalledWith(36, callback);
+  });
+
+  it('should get an alarm without a given callback', function(){
+    chromeService.alarms.get(36);
+    expect(chrome.alarms.get).toHaveBeenCalledWith(36, jasmine.any(Function));
+  });
+
+  it('should return a promise when getting an alarm', function(){
+    var promise = chromeService.alarms.get(36);
+    expect(promise.reject).toBeDefined();
+    expect(promise.resolve).toBeDefined();
+  });
+
+  it('should resolve a promise on getting an alarm when no callback have been given ', function(){
+    chrome.alarms.get = function(id, callback){
+      callback(SUCCESS_GET_ALARM_RESPONSE);
+    };
+
+    var alarm;
+    var promise = chromeService.alarms.get(36);
+
+    promise.then(function(response){
+      alarm = {
+        id: response.id,
+        value: response.value
+      };
+    });
+
+    expect(alarm.id).toEqual(36);
+    expect(alarm.value).toEqual('63');
+  });
+
+  it('should clear an alarm with a given id and a callback', function(){
+    var callback = function(){};
+    chromeService.alarms.clear(36, callback);
+    expect(chrome.alarms.clear).toHaveBeenCalledWith(36, callback);
+  });
+
+  it('should clear an alarm with just a given id', function(){
+    chromeService.alarms.clear(36);
+    expect(chrome.alarms.clear).toHaveBeenCalledWith(36, jasmine.any(Function));
+  });
+
+  it('should return nothing when clearing an alarm with no callback given', function(){
+    chrome.alarms.clear = function(id, callback){
+      callback();
+    };
+
+    var blankReturn = chromeService.alarms.clear(36);
+    expect(blankReturn).toEqual(undefined);
+  });
+
+});
