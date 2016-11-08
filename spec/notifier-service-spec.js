fed8928b2b23a27b26dcb580a377b4c3b5efe24c
Rafael Camargo <contato@rafaelcamargo.com>
2016-11-07 23:39:45 -0200

    Adding unit tests to notifier service

--- spec/notifier-service-spec.js
+++ spec/notifier-service-spec.js
@@ -1,5 +1,30 @@
 describe('Notifier Service', function(){
 
+  var stubStorageGetAlerts,
+    stubAlarmsGet;
+
+  beforeEach(function(){
+    stubStorageGetAlerts = function(resolvedResponse){
+      chromeService.storage.getAlerts = function(){
+        return {
+          then: function(callback){
+            callback(resolvedResponse);
+          }
+        }
+      }
+    };
+
+    stubAlarmsGet = function(resolvedResponse){
+      chromeService.alarms.get = function(){
+        return {
+          then: function(callback){
+            callback(resolvedResponse);
+          }
+        }
+      }
+    }
+  });
+
   it('should set listeners on module initialisation', function(){
     spyOn(chromeService.alarms, 'addListener');
     spyOn(chromeService.storage, 'onChanged');
@@ -10,15 +35,34 @@ describe('Notifier Service', function(){
   });
 
   it('should check saved alerts on module initialisation', function(){
-    spyOn(chromeService.storage, 'getAlerts').and.returnValue({
-      then: function(){}
-    });
+    stubStorageGetAlerts([]);
+    spyOn(chromeService.storage, 'getAlerts').and.callThrough();
 
     notifierService.init();
     expect(chromeService.storage.getAlerts).toHaveBeenCalled();
   });
 
-  it('should request USD exchange value', function(){
+  it('should schedule currency service request when there is some alert stored', function(){
+    stubStorageGetAlerts([3.60]);
+    stubAlarmsGet(undefined);
+    spyOn(chromeService.alarms, 'create');
+
+    notifierService.init();
+    expect(chromeService.alarms.create).toHaveBeenCalledWith('dollertAlarm', {
+      periodInMinutes: 1
+    });
+  });
+
+  it('should not schedule currency service request when there is some alert already created', function(){
+    stubStorageGetAlerts([3.60]);
+    stubAlarmsGet({});
+    spyOn(chromeService.alarms, 'create');
+
+    notifierService.init();
+    expect(chromeService.alarms.create).not.toHaveBeenCalled();
+  });
+
+  it('should request USD exchange value on module initialisation when there is some alert stored', function(){
     spyOn(currencyService, 'getCurrentUSDValue').and.returnValue({
       then: function(){}
     });
